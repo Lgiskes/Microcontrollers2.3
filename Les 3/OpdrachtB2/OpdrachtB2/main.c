9091d79b6ece4b5f3f956b3c0d24c09e583d42ad
Jan Kees <jc.broekhuizen@student.avans.nl>
2021-02-24 12:51:51 +0100

    [Add] Week 3 | exercise B2

---
+++ Les 3/OpdrachtB2/OpdrachtB2/main.c
@@ -0,0 +1,178 @@
+/*
+ * main.c
+ *
+ * Created: 2/24/2021 11:02:59 AM
+ *  Author: jkbro
+ */ 
+
+#define F_CPU 8e6
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <stdlib.h>
+
+#define BIT(x)	(1 << (x))
+#define LCD_E 	6  // RA6 UNI-6
+#define LCD_RS	4  // RA4 UNI-6
+
+void lcd_strobe_lcd_e(void);
+void lcd_write_data(unsigned char byte);
+void lcd_write_cmd(unsigned char byte);
+void lcd_clear(void);
+void display_text(const char *str);
+void set_cursor(int position);
+void init(void);
+
+void wait( int ms ) {
+	for (int tms=0; tms<ms; tms++) {
+		_delay_ms( 1 );			// library function (max 30 ms at 8MHz)
+	}
+}
+
+char * toArray(int number)
+{
+    int n = log10(number) + 1;
+    int i;
+    char *numberArray = calloc(n, sizeof(char));
+    for (i = n-1; i >= 0; --i, number /= 10)
+    {
+        numberArray[i] = (number % 10) + '0';
+    }
+    return numberArray;
+}
+
+int main(void)
+{
+	DDRD &= ~BIT(7);
+	PORTC = 0xFF;
+	DDRB = 0xFF;
+	
+	TCCR2 = 0b00000111;
+	init();
+	wait(10);
+	int oldCounter = 0;
+	//int passedHundred = 0;
+	oldCounter = TCNT2;
+	
+	lcd_clear();
+	wait(10);
+	
+	set_cursor(0);
+	wait(5);
+	display_text(toArray(oldCounter));
+	wait(5);
+	while(1)
+	{
+		//TODO:: Please write your application code
+		if (oldCounter != TCNT2) {
+			/*if (TCNT2 == 100)
+			{
+				passedHundred++;
+				TCNT2 = 0;
+			}*/
+			oldCounter = TCNT2;
+			lcd_clear();
+			wait(5);
+			display_text(toArray(oldCounter));
+			wait(5);
+		}
+		PORTB = TCNT2;
+		wait(10);
+	}
+}
+
+void lcd_strobe_lcd_e(void) {
+	PORTA |= (1<<LCD_E);	// E high
+	_delay_ms(1);			// nodig
+	PORTA &= ~(1<<LCD_E);  	// E low
+	_delay_ms(1);			// nodig?
+}
+
+void init(void) {
+	// PORTC output mode and all low (also E and RS pin)
+	//DDRD = 0xFF;
+	DDRC = 0xFF;
+	DDRA = 0xFF;
+	PORTC = 0x00;
+	PORTA = 0x00;
+	//PORTA = 0xFF;
+
+	// Step 2 (table 12)
+	PORTC = 0x20;	// function set
+	lcd_strobe_lcd_e();
+
+	// Step 3 (table 12)
+	PORTC = 0x20;   // function set
+	lcd_strobe_lcd_e();
+	PORTC = 0x80;
+	lcd_strobe_lcd_e();
+
+	// Step 4 (table 12)
+	PORTC = 0x00;   // Display on/off control
+	lcd_strobe_lcd_e();
+	PORTC = 0xF0;
+	lcd_strobe_lcd_e();
+
+	// Step 4 (table 12)
+	PORTC = 0x00;   // Entry mode set
+	lcd_strobe_lcd_e();
+	PORTC = 0x60;
+	lcd_strobe_lcd_e();
+}
+
+void display_text(const char *str) {
+	// Het kan met een while:
+
+	// while(*str) {
+	// 	lcd_write_data(*str++);
+	// }
+
+	// of met een for:
+	for(;*str; str++){
+		lcd_write_data(*str);
+	}
+}
+
+void lcd_write_data(unsigned char byte) {
+	// First nibble.
+	PORTC = byte;
+	PORTA |= (1<<LCD_RS);
+	lcd_strobe_lcd_e();
+
+	// Second nibble
+	PORTC = (byte<<4);
+	PORTA |= (1<<LCD_RS);
+	lcd_strobe_lcd_e();
+}
+
+void lcd_write_command(unsigned char byte) {
+	// First nibble.
+	PORTC = byte;
+	PORTA &= ~(1<<LCD_RS);
+	lcd_strobe_lcd_e();
+
+	// Second nibble
+	PORTC = (byte<<4);
+	PORTA &= ~(1<<LCD_RS);
+	lcd_strobe_lcd_e();
+}
+
+void lcd_clear() {
+	lcd_write_command (0x01);						//Leeg display
+	_delay_ms(2);
+	lcd_write_command (0x80);						//Cursor terug naar start
+}
+
+void set_cursor(int position){
+	if(position >= 0 && position <= 15){
+		position += 0x80;
+		lcd_write_command(position);
+	}
+	else{
+		if(position >= 16 && position <= 31){
+			position+=0x80+48;
+			lcd_write_command(position);
+		}
+	}	
+}
