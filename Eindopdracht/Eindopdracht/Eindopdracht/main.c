ffedf3af9341bbdae1c29324a1fe8e8937925ef1
Jan Kees <jc.broekhuizen@student.avans.nl>
2021-03-17 10:59:49 +0100

    [Add]: setup project eindopdracht

---
+++ Eindopdracht/Eindopdracht/Eindopdracht/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 3/17/2021 10:58:48 AM
+ *  Author: jkbro
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
